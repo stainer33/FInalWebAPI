a591a8f6fead4b72b8e3d52b32f61394972457b0
stainer33 <spasang91@gmail.com>
2020-01-30 05:39:35 +0545

    upload image

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var express = require ('express');
 var app = express();
 var BodyParser = require('body-parser');
 var UserController = require('./Controllers/UserController');
+var uploadRouter=require('./Controllers/upload');
 
-
+app.post('/signup',  UserController.Hashing,UserController.CheckIfExist,UserController.Registration);
+app.post('/login',UserController.Hashing,UserController.Login,AuthController.jwtTokenGen);
+app.use('/upload', uploadRouter);
+app.get('/profile/:email',UserController.GetAll);
 
 
 
