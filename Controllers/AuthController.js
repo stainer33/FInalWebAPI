967edfa0967562f7b0f3fb142abe54d94d2a57e8
stainer33 <spasang91@gmail.com>
2020-01-30 05:24:26 +0545

    Generating jwt token

---
+++ Controllers/AuthController.js
@@ -0,0 +1,18 @@
+var jwt = require('jsonwebtoken');
+
+
+
+
+
+//generating token
+function jwtTokenGen(req, res,next)
+{	var myPayload = {username: req.body.username,
+                    userLevel: 'superadmin'};
+    jwt.sign( myPayload, 'secret', { expiresIn: '1h' },function(err, token)
+    {if(err){ console.log(err);res.json(err);}
+    else{console.log(token);
+        
+        res.json({status: 201, token: token});
+    }});
+
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require ('express');
 var app = express();
+var BodyParser = require('body-parser');
 var UserController = require('./Controllers/UserController');
 
 
