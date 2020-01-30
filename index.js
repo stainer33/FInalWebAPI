0409ac6a07b5f077773309b5ac2acd8e0de03c73
stainer33 <spasang91@gmail.com>
2020-01-30 05:47:50 +0545

    getting uploaded image

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require ('express');
 var app = express();
+var fs=require('fs');
 var BodyParser = require('body-parser');
 var UserController = require('./Controllers/UserController');
 var uploadRouter=require('./Controllers/upload');
+
+app.use(BodyParser.urlencoded({extended: true}));
 
 app.post('/signup',  UserController.Hashing,UserController.CheckIfExist,UserController.Registration);
 app.post('/login',UserController.Hashing,UserController.Login,AuthController.jwtTokenGen);
 app.use('/upload', uploadRouter);
 app.get('/profile/:email',UserController.GetAll);
+app.get('/image/:image', (req, res) => {
+    pic=req.params.image
+    res.sendFile('./public/uploads/'+pic, { root: __dirname });
+});
 
 
 
